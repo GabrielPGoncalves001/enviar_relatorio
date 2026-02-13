4dfb4ccf72b121c6145639ae24b699c26de636b6
Gabriel Gonçalves <gabrielp.goncalves1@gmail.com>
2026-02-13 16:32:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 img = 'assets/cachorro_foda.png'
-
+st.image()
 with open(img, "rb") as f:
   img_b = f.read()
 
 st.download_button(
-  label="carro",
+  label="CLIQUE AQUI IMEDIATAMENTE",
   data=img_b,
   file_name="carro.png",
   mime="image/png")
